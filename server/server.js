305e065cc5a020a8140140b836704d49a1d5a735
Abdelsalam Mahari <abdelsalam.mahari@gmail.com>
2023-09-06 23:42:17 +0300

    add student by  abdelsalam

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const signupRoutes = require('./routes/SignupRoute');
+const loginRoutes = require('./routes/LoginRoute');
 
 require('dotenv').config();
 
@@ -12,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use(signupRoutes);
+app.use(signupRoutes, loginRoutes);
 
 mongoose.connect(process.env.MONGODB_URL)
 .then(() => console.log('Connected To MongoDB, Server Works!'))

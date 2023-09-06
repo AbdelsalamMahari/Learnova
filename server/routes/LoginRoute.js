ea70648c91a44b089a74a629e841ab25df9cd205
Abdelsalam Mahari <abdelsalam.mahari@gmail.com>
2023-09-06 23:44:25 +0300

    add student by abdelsalam

--- server/routes/LoginRoute.js
+++ server/routes/LoginRoute.js
@@ -3,6 +3,6 @@ const { login } = require('../controllers/LoginController');
 
 const router = Router();
 
-router.post('/login', login);
+router.post('/login/student', login);
 
 module.exports = router;
--- server/routes/SignupRoute.js
+++ server/routes/SignupRoute.js
@@ -4,7 +4,7 @@ const { signup, link } = require('../controllers/SignupController');
 const router = Router();
 
 router.get('/:id/verify/:token/', link);
-router.post('/signup', signup);
+router.post('/signup/student', signup);
 
 
 module.exports = router;

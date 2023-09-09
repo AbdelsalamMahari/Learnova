1b2a4151b3ca1f3ef21ed050cd4f632a540d89fa
Abdelsalam Mahari <abdelsalam.mahari@gmail.com>
2023-09-09 15:19:52 +0300

    Add about section

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
+import Navbar from "../../layout/navbar/Navbar"
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      <div className="scroll-container">
+        <section className="backdrop-image-all">
+          <Navbar/>
+          <div className="backdrop-section-all">
+            <div className=" px-[30px]">
+              <h1 className="lg:text-6xl font-bold text-white my-8">About Us</h1>
+              <p className="text-lg text-white">Your portal to a world of knowledge and growth. Explore, learn, and thrive with Learnova</p>
+              </div>
+          </div>
+        </section>
+        <section className="h-screen">hii</section>
+      </div>
+    </>
+  );
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Home.css";
+import Navbar from "../../layout/navbar/Navbar"
 
 export default function Home() {
   return (
     <>
       <div className="scroll-container">
         <section className="backdrop-image">
+          <Navbar/>
           <div className="backdrop-section">
-            <div className="w-1/2">
-              <h1 className="lg:text-7xl font-bold text-white my-8">Learning Fuels Creative Thinking</h1>
+            <div className=" px-[30px]">
+              <h1 className="lg:text-6xl font-bold text-white my-8">Learning Fuels Creative Thinking</h1>
               <p className="text-lg text-white">Your portal to a world of knowledge and growth. Explore, learn, and thrive with Learnova</p>
               </div>
           </div>

f87e810726ed156660be60520f9904bbcee90f39
wafik <safawafik@gmail.com>
2023-09-10 23:59:12 +0300

    update by wafik

--- client/src/assets/icons/icons.jsx
+++ client/src/assets/icons/icons.jsx
@@ -1,7 +1,14 @@
-import { FaFacebook } from 'react-icons/fa'
+import { FaFacebook, FaCrown } from 'react-icons/fa';
+import { PiCertificateBold } from 'react-icons/pi';
+import { BsClock } from 'react-icons/bs';
+
 
 const Icons = {
-    Facebook:FaFacebook
+    Facebook:FaFacebook,
+    Certificate:PiCertificateBold,
+    Clock:BsClock,
+    Crown:FaCrown,
+
 }
 
 export default Icons;
--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./About.css";
 import TopPage from "../../components/topPage/TopPage";
+import Icons from "../../assets/icons/icons";
 
 export default function About() {
+  
   return (
     <>
       <TopPage
@@ -28,11 +30,76 @@ export default function About() {
           </div>
           <div className="second2-about">
             <h1>Learn Something Every Day</h1>
-            <hr />
+            <div className="tilted-box" style={{ '--skew-angle': '40deg' }}></div>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam minima autem libero reiciendis fuga mollitia alias molestias iure, fugiat consectetur dolorem repellendus tempora, ipsam magni deserunt.</p>
           </div>
         </div>
       </section>
+      <section className="section4-about">
+        <div className="container3-about">
+          <div className="first3-about">
+            <h3>Our Vision</h3>
+            <h1>Who We Are</h1>
+          </div>
+          <div className="second3-about">
+            <h2>"It is impossible for a man to learn what he thinks he already knows."</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam minima autem libero reiciendis fuga mollitia alias molestias iure, fugiat consectetur dolorem repellendus tempora.</p>
+            <div className="second2-about-pro">
+              <div className="first-div-about">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam minima autem libero reiciendis fuga mollitia alias molestias iure, fugiat consectetur dolorem repellendus tempora.</p>
+              </div>
+              <div className="second-div-about">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam minima autem libero reiciendis fuga mollitia alias molestias iure, fugiat consectetur dolorem repellendus tempora.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section4 p-[30px] text-white">
+        <div className="relative z-10 my-10">
+          <div className="flex gap-5">
+            <div className="flex-1">
+              <Icons.Crown className="icons-about"/>
+              <h1 className="text-2xl font-bold my-3">Learn From Industry Leaders</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consec tetur adipis cing elit. Ut
+                elit tellus, luctus nec ullam corper mattis, pulvinar dapibus.
+              </p>
+            </div>
+            <div className="flex-1">
+              <Icons.Clock className="icons-about"/>
+              <h1 className="text-2xl font-bold my-3">Learn at Your Own Pace</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consec tetur adipis cing elit. Ut
+                elit tellus, luctus nec ullam corper mattis, pulvinar dapibus.
+              </p>
+            </div>
+            <div className="flex-1">
+              <Icons.Certificate className="icons-about"/>
+              <h1 className="text-2xl font-bold my-3">Professional Certification</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consec tetur adipis cing elit. Ut
+                elit tellus, luctus nec ullam corper mattis, pulvinar dapibus.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="feedback-container">
+          <div className="feedback-form">
+            <h2>
+              <b>Give Us Your Feedback</b>
+            </h2>
+            <form onSubmit="">
+              <textarea
+                placeholder="Your feedback..."
+                value=""
+                onChange=""
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </section>
     </>
   );
 }

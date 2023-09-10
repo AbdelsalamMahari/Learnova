57872a5b471ad854382fe5b2212bd83d01ce5264
Abdelsalam Mahari <abdelsalam.mahari@gmail.com>
2023-09-10 13:32:24 +0300

    update home.jsx by abdelsalam

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -10,18 +10,19 @@ export default function Home() {
         <section className="backdrop-image">
           <Navbar />
           <div className="backdrop-section">
-            <div className=" px-[30px]">
+            <div className="px-[30px]">
               <h1 className="lg:text-6xl font-bold text-white my-8">
                 Learning Fuels Creative Thinking
               </h1>
-              <p className="text-lg text-white">
+              <p className="text-lg text-white my-8">
                 Your portal to a world of knowledge and growth. Explore, learn,
                 and thrive with Learnova
               </p>
+              <Button text={"START COURSE"}></Button>
             </div>
           </div>
         </section>
-        <section className="h-screen">
+        <section className="min-h-screen">
           <div className="flex p-[30px]">
             <div className="w-[35%]">
               <h4>Top Categories</h4>
@@ -80,7 +81,7 @@ export default function Home() {
               <div className="flex flex-col p-5 gap-5 bg-white border">
                 <div>
                   <h1 className="lg:text-4xl w-full h-[80px]">
-                  WordPress Basic Tutorial
+                    WordPress Basic Tutorial
                   </h1>
                 </div>
                 <div>
@@ -110,7 +111,7 @@ export default function Home() {
               <div className="flex flex-col p-5 gap-5 bg-white border">
                 <div>
                   <h1 className="lg:text-4xl w-full h-[80px]">
-                  E-Commerce Course
+                    E-Commerce Course
                   </h1>
                 </div>
                 <div>
@@ -131,8 +132,40 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="therd-section">
-          <div className="py-[120px] relative z-10">hello</div>
+        <section className="therd-section p-[30px] text-white">
+          <div className="relative z-10 my-10">
+            <div className="flex gap-5">
+              <div className="flex-1">
+                <div className="border-4 w-1/4 border-dashed border-orange transform skew-x-12"></div>
+                <h1 className="text-2xl font-bold my-3">Experience</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consec tetur adipis cing elit. Ut
+                  elit tellus, luctus nec ullam corper mattis, pulvinar dapibus.
+                </p>
+              </div>
+              <div className="flex-1">
+              <div className="border-4 w-1/4 border-dashed border-orange transform skew-x-12"></div>
+                <h1 className="text-2xl font-bold my-3">Education</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consec tetur adipis cing elit. Ut
+                  elit tellus, luctus nec ullam corper mattis, pulvinar dapibus.
+                </p>
+              </div>
+              <div className="flex-1">
+              <div className="border-4 w-1/4 border-dashed border-orange transform skew-x-12"></div>
+                <h1 className="text-2xl font-bold my-3">Certificate</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consec tetur adipis cing elit. Ut
+                  elit tellus, luctus nec ullam corper mattis, pulvinar dapibus.
+                </p>
+              </div>
+              <div className="bg-white text-black p-5 w-96">
+                <h3>Study at Your Own Pace</h3>
+                <h1 className="lg:text-2xl font-bold my-4">Boost Your Career by Learning Skills in High Demand</h1>
+                <a href="/">GET STARTED</a>
+              </div>
+            </div>
+          </div>
         </section>
         <section className="h-screen">
           <div>hello</div>

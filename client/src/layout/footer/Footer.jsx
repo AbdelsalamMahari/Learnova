efc64c45a56d5d91591177d62948f6f8a8cdf82c
Abdelsalam Mahari <abdelsalam.mahari@gmail.com>
2023-09-11 16:38:24 +0300

    update by abdelsalam

--- client/src/layout/footer/Footer.jsx
+++ client/src/layout/footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <>
       <footer className="bg-white text-black ">
-        <div className="container mx-auto p-[30px]">
+        <div className="container  p-[30px]">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6 py-4">
             {/* Footer Section 1 */}
             <div className=" md:col-span-2">

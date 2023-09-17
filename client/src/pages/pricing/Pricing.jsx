6643fd4a8553d7ce81a369e6600bb5f0679d86ff
Abdelsalam Mahari <abdelsalam.mahari@gmail.com>
2023-09-17 18:35:35 +0300

    add pricing plans

--- client/src/pages/pricing/Pricing.jsx
+++ client/src/pages/pricing/Pricing.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Pricing.css"; // You can remove this line if not needed
 import TopPage from "../../components/topPage/TopPage";
+import Footer from "../../layout/footer/Footer";
+import Icons from "../../assets/icons/icons";
 
 export default function Pricing() {
   return (
@@ -9,29 +11,137 @@ export default function Pricing() {
         title="Pricing"
         backgroundImageUrl="https://websitedemos.net/online-courses-02/wp-content/uploads/sites/542/2020/02/bg-05-free-img.jpg"
       />
-      <div className="container mx-auto mt-8">
+      <div className="container mx-auto my-10">
         <div className="flex justify-center space-x-8">
+          <div className="bg-white rounded-lg px-8 py-10 shadow-2xl text-center">
+            <h2 className="">Free</h2>
+            <p className="my-4">
+              <span className="text-4xl font-semibold">0$</span> /Month
+            </p>
+            <div
+              className="tilted-box2 w-1/4 m-auto"
+              style={{ "--skew-angle": "40deg" }}
+            ></div>
+            <div className="text-left my-3 leading-8">
+              <ul>
+              <li className="flex gap-1 items-center">
+                  <span>
+                    <Icons.Check size={20} />
+                  </span>
+                  Read All Chapters
+                </li>
+                <li className="flex gap-1 items-center">
+                  <span>
+                    <Icons.Check size={20} />
+                  </span>
+                  Exercise Files
+                </li>
+                <li className="flex gap-1 items-center text-gray-400">
+                  <span>
+                    <Icons.Close size={20} />
+                  </span>
+                  Course Learning Checks
+                </li>
+                <li className="flex gap-1 items-center text-gray-400">
+                  <span>
+                    <Icons.Close size={20} />
+                  </span>
+                  Certificate of Completion
+                </li>
+              </ul>
+            </div>
+            <button className="mt-4 bg-orange hover:bg-blue-700 text-white py-2 px-4 rounded-full">
+              Get Started
+            </button>
+          </div>
           {/* Plan 1 */}
-          <div className="bg-white rounded-lg p-4 shadow-md">
-            <h2 className="text-2xl font-semibold">Monthly Plan</h2>
-            <p className="text-gray-600">Access to our content for 30 days</p>
-            <p className="text-3xl font-bold text-blue-600">$9.99</p>
-            <button className="mt-4 bg-blue hover:bg-blue-700 text-white py-2 px-4 rounded-full">
+          <div className="bg-white rounded-lg px-8 py-10 shadow-2xl text-center">
+            <h2 className="">Monthly</h2>
+            <p className="my-4">
+              <span className="text-4xl font-semibold">9.99$</span> /Month
+            </p>
+            <div
+              className="tilted-box2 w-1/4 m-auto"
+              style={{ "--skew-angle": "40deg" }}
+            ></div>
+            <div className="text-left my-3 leading-8">
+              <ul>
+              <li className="flex gap-1 items-center">
+                  <span>
+                    <Icons.Check size={20} />
+                  </span>
+                  Read All Chapters
+                </li>
+                <li className="flex gap-1 items-center">
+                  <span>
+                    <Icons.Check size={20} />
+                  </span>
+                  Exercise Files
+                </li>
+                <li className="flex gap-1 items-center">
+                  <span>
+                    <Icons.Check size={20} />
+                  </span>
+                  Course Learning Checks
+                </li>
+                <li className="flex gap-1 items-center text-gray-400">
+                  <span>
+                    <Icons.Close size={20} />
+                  </span>
+                  Certificate of Completion
+                </li>
+              </ul>
+            </div>
+            <button className="mt-4 bg-orange hover:bg-blue-700 text-white py-2 px-4 rounded-full">
               Purchase
             </button>
           </div>
 
           {/* Plan 2 */}
-          <div className="bg-white rounded-lg p-4 shadow-md">
-            <h2 className="text-2xl font-semibold">Annual Plan</h2>
-            <p className="text-gray-600">Access to our content for 365 days</p>
-            <p className="text-3xl font-bold text-blue-600">$99.99</p>
-            <button className="mt-4 bg-blue hover:bg-blue-700 text-white py-2 px-4 rounded-full">
+          <div className="bg-blue text-white rounded-lg px-8 py-10 shadow-2xl text-center">
+            <h2 className="">Annual</h2>
+            <p className="my-4">
+              <span className="text-4xl font-semibold">99.99$</span> /Year
+            </p>
+            <div
+              className="tilted-box2 w-1/4 m-auto"
+              style={{ "--skew-angle": "40deg" }}
+            ></div>
+            <div className="text-left my-3 leading-8">
+              <ul>
+              <li className="flex gap-1 items-center">
+                  <span>
+                    <Icons.Check size={20} />
+                  </span>
+                  Read All Chapters
+                </li>
+                <li className="flex gap-1 items-center">
+                  <span>
+                    <Icons.Check size={20} />
+                  </span>
+                  Exercise Files
+                </li>
+                <li className="flex gap-1 items-center">
+                  <span>
+                    <Icons.Check size={20} />
+                  </span>
+                  Course Learning Checks
+                </li>
+                <li className="flex gap-1 items-center">
+                  <span>
+                    <Icons.Check size={20} />
+                  </span>
+                  Certificate of Completion
+                </li>
+              </ul>
+            </div>
+            <button className="mt-4 bg-orange hover:bg-blue-700 text-white py-2 px-4 rounded-full">
               Purchase
             </button>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 }

c437657300e2b550ef78419d38c1b685e21dd3ea
moefawazz <hamoudirachido@gmail.com>
2023-09-21 02:38:44 +0300

    fetching users courses in edit course

--- client/src/pages/instructorDash/CourseEdit.jsx
+++ client/src/pages/instructorDash/CourseEdit.jsx
@@ -2,29 +2,31 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Sidebar from "../../components/sidebars/InstructorSideBar";
 import Icons from "../../assets/icons/icons";
-import ImageUpload from "../../components/ImageUpload/ImageUpload"; 
-import UserInfo from '../../components/users/UserInfo';
 import { fetchUserInfoFromToken } from "../../utils/fetchUser/FetchUser";
 
 export default function CourseEdit() {
   const [user, setUser] = useState(null);
   const [courses, setCourses] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       const userInfo = await fetchUserInfoFromToken();
       setUser(userInfo);
+      console.log(userInfo);
 
-      axios.get("http://localhost:5000/courses/")
+      axios
+        .get("http://localhost:5000/courses/")
         .then((response) => {
-          // Filter courses where instructor matches user._id
           const filteredCourses = response.data.filter(
             (course) => course.instructor === userInfo._id
           );
           setCourses(filteredCourses);
+          setIsLoading(false);
         })
         .catch((error) => {
           console.error("Error fetching courses:", error);
+          setIsLoading(false);
         });
     }
 
@@ -38,18 +40,23 @@ export default function CourseEdit() {
         <div className="toggle">
           <Icons.Bars size={24} />
         </div>
-        
+
         <div className="course-list">
-          <h2>Your Courses</h2>
-          <ul>
-            {courses.map((course) => (
-              <li key={course._id}>
-                <h3>{course.name}</h3>
-                <p>{course.description}</p>
-                {/* Add links or buttons for editing or managing the course */}
-              </li>
-            ))}
-          </ul>
+          {isLoading ? (
+            <p>Loading content...</p>
+          ) : (
+            <>
+              <h2>{user.firstName + " " + user.lastName} courses</h2>
+              <ul>
+                {courses.map((course) => (
+                  <li key={course._id}>
+                    <h3>{course.name}</h3>
+                    <p>{course.description}</p>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
         </div>
       </div>
     </div>

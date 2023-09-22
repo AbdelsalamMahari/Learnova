35d8027710febaabc440c05f0d3e68d33b113435
Abdelsalam Mahari <abdelsalam.mahari@gmail.com>
2023-09-22 13:04:50 +0300

    update exam

--- client/src/pages/courseStart/CourseExam.jsx
+++ client/src/pages/courseStart/CourseExam.jsx
@@ -23,12 +23,10 @@ export default function CourseExam() {
 
           // Fetch questions for the course
           const questionsResponse = await axios.get(`http://localhost:5000/exams/course/${courseId}`);
-          const filteredQuestions = questionsResponse.data.filter((question) => !hasBeenAnswered(question._id)); // Filter out answered questions
-          setQuestions(filteredQuestions);
+          setQuestions(questionsResponse.data);
 
-          // Fetch user's score for this course from the database
-          const userScoreResponse = await axios.get(`http://localhost:5000/${user._id}/examScore/${courseId}`);
-          setScore(userScoreResponse.data.score);
+          // Initialize userAnswers state with empty values
+          setUserAnswers(new Array(questionsResponse.data.length).fill(undefined));
         } catch (error) {
           console.error(error);
         }
@@ -44,46 +42,22 @@ export default function CourseExam() {
     setUserAnswers(updatedAnswers);
   };
 
-  const checkAnswer = async (questionIndex) => {
-    if (userAnswers[questionIndex] === undefined) {
-      return;
-    }
+  const handleSubmitAnswers = async () => {
+    // Calculate the score based on user answers
+    const updatedScore = userAnswers.reduce((acc, answer, index) => {
+      if (answer !== undefined && questions[index].options[answer].isTrue) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
 
-    const selectedOptionIndex = userAnswers[questionIndex];
-    const selectedOption = questions[questionIndex].options[selectedOptionIndex];
+    // Update the user's score in the state
+    setScore(updatedScore);
 
-    if (selectedOption.isTrue) {
-      const updatedScore = score + 1;
-      setScore(updatedScore);
-
-      // Mark the question as answered
-      const updatedQuestions = [...questions];
-      updatedQuestions[questionIndex].answered = true;
-      setQuestions(updatedQuestions);
-
-      // Store that this question has been answered correctly in localStorage
-      markAsAnswered(questions[questionIndex]._id);
-      
-      // Update the user's score in the database
-      await axios.put(`http://localhost:5000/${user._id}/examScore/${courseId}`, {
-        score: updatedScore,
-      });
-    }
-  };
-
-  const remainingQuestions = questions.slice(0, 5);
-
-  // Helper function to mark a question as answered in localStorage
-  const markAsAnswered = (questionId) => {
-    const answeredQuestions = JSON.parse(localStorage.getItem('answeredQuestions')) || [];
-    answeredQuestions.push(questionId);
-    localStorage.setItem('answeredQuestions', JSON.stringify(answeredQuestions));
-  };
-
-  // Helper function to check if a question has been answered
-  const hasBeenAnswered = (questionId) => {
-    const answeredQuestions = JSON.parse(localStorage.getItem('answeredQuestions')) || [];
-    return answeredQuestions.includes(questionId);
+    // Update the user's score in the database
+    await axios.put(`http://localhost:5000/${user._id}/examScore/${courseId}`, {
+      score: updatedScore,
+    });
   };
 
   return (
@@ -98,45 +72,33 @@ export default function CourseExam() {
           </div>
           <div className='course-quiz p-4 bg-white rounded-lg shadow-lg'>
             <h1 className="text-2xl font-bold mb-4">Questions for Course: {course.name || 'Loading...'}</h1>
-            
-            {score >= 5 ? (
-              <p className="text-xl text-green-500 font-semibold mb-4">Ready for your exam!</p>
-            ) : (
-              <ul className="list-disc pl-4">
-                {remainingQuestions.map((question, questionIndex) => (
-                  <li key={question._id} className="mb-4">
-                    {hasBeenAnswered(question._id) ? (
-                      <p className="text-green-500">This question has been answered correctly.</p>
-                    ) : (
-                      <>
-                        <h3 className="text-lg font-semibold">{question.questionText}</h3>
-                        <ul className="list-disc pl-4">
-                          {question.options.map((option, optionIndex) => (
-                            <li key={optionIndex} className="flex items-center space-x-2">
-                              <input
-                                type="radio"
-                                checked={userAnswers[questionIndex] === optionIndex}
-                                onChange={() => handleSelectAnswer(questionIndex, optionIndex)}
-                                className="h-5 w-5 text-blue-500"
-                                disabled={hasBeenAnswered(question._id)}
-                              />
-                              <span>{option.text}</span>
-                            </li>
-                          ))}
-                        </ul>
-                        <button
-                          onClick={() => checkAnswer(questionIndex)}
-                          className="bg-green-500 text-white px-4 py-2 mt-4 rounded-md hover:bg-green-600"
-                          disabled={userAnswers[questionIndex] === undefined}
-                        >
-                          Check Answer
-                        </button>
-                      </>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            )}
+            <ul className="list-disc pl-4">
+              {questions.map((question, questionIndex) => (
+                <li key={question._id} className="mb-4">
+                  <h3 className="text-lg font-semibold">{question.questionText}</h3>
+                  <ul className="list-disc pl-4">
+                    {question.options.map((option, optionIndex) => (
+                      <li key={optionIndex} className="flex items-center space-x-2">
+                        <input
+                          type="radio"
+                          checked={userAnswers[questionIndex] === optionIndex}
+                          onChange={() => handleSelectAnswer(questionIndex, optionIndex)}
+                          className="h-5 w-5 text-blue-500"
+                        />
+                        <span>{option.text}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+            <button
+              onClick={handleSubmitAnswers}
+              className="bg-green-500 text-white px-4 py-2 mt-4 rounded-md hover:bg-green-600"
+              disabled={userAnswers.includes(undefined)}
+            >
+              Submit Answers
+            </button>
             <p className="mt-2">Score: {score}</p>
           </div>
         </div>

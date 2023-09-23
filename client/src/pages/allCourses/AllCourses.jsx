c31b818b25afad5c0206205b9983eb8568eeb022
wafik <safawafik@gmail.com>
2023-09-24 00:27:17 +0300

    update by wafik

--- client/src/pages/allCourses/AllCourses.jsx
+++ client/src/pages/allCourses/AllCourses.jsx
@@ -8,18 +8,18 @@ import Footer from "../../layout/footer/Footer"
 
 export default function AllCourses() {
   const [courses, setCourses] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); 
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/courses") 
+      .get("http://localhost:5000/courses")
       .then((response) => {
         setCourses(response.data);
-        setIsLoading(false); 
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching course data:", error);
-        setIsLoading(false); 
+        setIsLoading(false);
       });
   }, []);
 
@@ -30,45 +30,42 @@ export default function AllCourses() {
         backgroundImageUrl="https://websitedemos.net/online-courses-02/wp-content/uploads/sites/542/2020/02/bg-08-free-img.jpg"
       />
 
-      {isLoading ? ( 
-       <Loading/>
+      {isLoading ? (
+        <Loading />
       ) : (
-        <div className="p-[20px] gap-6 cont-course grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        // Updated allcourse.jsx with class names from instructors.jsx
+        <div className="section2-instructor">
           {courses.map((course, index) => (
-            <div key={index} className="flex-1 shadow-2xl">
-              <div>
-              {course.backdrop ? (
-
-                <img
-                  src={`http://localhost:5000/courses/getBackdrop/${course._id}`} // Use the local URL
-                  alt="Upload"
-                />
-              
-            ) : (
-              <img
-                src="https://images.nightcafe.studio//assets/profile.png?tr=w-1600,c-at_max"
-                alt="Default Profile"
-              />
-            )}
+            <div className="container2-instructor" key={index}>
+              <div className="first2-instructor">
+                {course.backdrop ? (
+                  <img
+                    src={`http://localhost:5000/courses/getBackdrop/${course._id}`}
+                    alt="Upload"
+                    className="w-28 h-28 rounded-full object-cover"
+                  />
+                ) : (
+                  <img
+                    src="https://images.nightcafe.studio//assets/profile.png?tr=w-1600,c-at_max"
+                    alt="Default Profile"
+                    className="w-28 h-28 rounded-full object-cover"
+                  />
+                )}
               </div>
-              <div className="flex flex-col p-3 gap-3 bg-white border">
-                <div>
-                  <h1 className="lg:text-2xl w-full">{course.name}</h1>
-                </div>
-                <div>
-                  <Link to={`/courseInfo/${course._id}`}>
-                    <button className="bg-blue rounded w-full p-1 text-white">
-                      See more...
-                    </button>
-                  </Link>
-                </div>
-                <div></div>
+              <div className="second2-instructor">
+                <h1>{course.name}</h1>
+                <Link to={`/courseInfo/${course._id}`}>
+                  <button className="bg-blue rounded w-full p-1 text-white">
+                    See more...
+                  </button>
+                </Link>
               </div>
             </div>
+
           ))}
         </div>
       )}
-      <Footer/>
+      <Footer />
     </>
   );
 }

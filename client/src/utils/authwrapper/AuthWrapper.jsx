95670dc10a100248ba6cb86be16fdc89503f99d1
moefawazz <hamoudirachido@gmail.com>
2023-09-24 13:51:52 +0300

    added protected routes wrapper

--- client/src/utils/authwrapper/AuthWrapper.jsx
+++ client/src/utils/authwrapper/AuthWrapper.jsx
@@ -1,38 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { fetchUserInfoFromToken } from "../../utils/fetchUser/FetchUser";
+import React, { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { fetchUserInfoFromToken } from '../../utils/fetchUser/FetchUser';
 
-function AuthWrapper() {
+function AuthWrapper({ element }) {
   const [isLoading, setIsLoading] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     async function checkAuthentication() {
-
       const userInfo = await fetchUserInfoFromToken();
-
       if (userInfo) {
         setIsLoggedIn(true);
       }
-
       setIsLoading(false);
     }
-
     checkAuthentication();
   }, []);
 
   if (isLoading) {
-
     return <div>Loading...</div>;
   }
 
-  if (!isLoggedIn) {
-
-    return <Navigate to="/login" />;
-  }
-
-
-  return <Outlet />;
+  return isLoggedIn ? element : <Navigate to="/login" />;
 }
 
 export default AuthWrapper;

bfbab964d6c8e7132c127ea4e683c2e417b72170
moefawazz <hamoudirachido@gmail.com>
2023-09-26 00:29:46 +0300

    added edit course

--- client/src/pages/instructorDash/CourseEdit.jsx
+++ client/src/pages/instructorDash/CourseEdit.jsx
@@ -8,12 +8,13 @@ export default function CourseEdit() {
   const [user, setUser] = useState(null);
   const [courses, setCourses] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [expandedCourse, setExpandedCourse] = useState(""); // Initialize with null
+  const [updatedCourses, setUpdatedCourses] = useState({}); // Track updated courses
 
   useEffect(() => {
     async function fetchData() {
       const userInfo = await fetchUserInfoFromToken();
       setUser(userInfo);
-      console.log(userInfo);
 
       axios
         .get("http://localhost:5000/courses/")
@@ -33,6 +34,64 @@ export default function CourseEdit() {
     fetchData();
   }, []);
 
+  const toggleCourseContent = (course) => {
+    if (expandedCourse === course._id) {
+      // Collapse the currently expanded course
+      setExpandedCourse(null);
+    } else {
+      // Expand the clicked course
+      setExpandedCourse(course._id);
+    }
+  };
+
+  const handleCloseCourseContent = () => {
+    setExpandedCourse(null);
+  };
+
+  const handleChangeImage = (event, courseId, chapterIndex, lessonIndex) => {
+    const file = event.target.files[0];
+    const fileName = file.name;
+    const updatedCoursesCopy = { ...updatedCourses };
+    const updatedCourseCopy = { ...updatedCoursesCopy[courseId] };
+
+    if (!updatedCourseCopy.content) {
+      updatedCourseCopy.content = [...courses.find((c) => c._id === courseId).content];
+    }
+
+    if (!updatedCourseCopy.content[chapterIndex]) {
+      updatedCourseCopy.content[chapterIndex] = { ...courses.find((c) => c._id === courseId).content[chapterIndex] };
+    }
+
+    if (!updatedCourseCopy.content[chapterIndex].lessons[lessonIndex]) {
+      updatedCourseCopy.content[chapterIndex].lessons[lessonIndex] = { ...courses.find((c) => c._id === courseId).content[chapterIndex].lessons[lessonIndex] };
+    }
+
+    updatedCourseCopy.content[chapterIndex].lessons[lessonIndex].image = fileName;
+    updatedCoursesCopy[courseId] = updatedCourseCopy;
+
+    setUpdatedCourses(updatedCoursesCopy);
+  };
+
+  const handleUpdateCourse = async (courseId) => {
+    const updatedCourse = updatedCourses[courseId];
+
+    if (updatedCourse) {
+      try {
+        const response = await axios.put(
+          `http://localhost:5000/courses/update/${courseId}`,
+          updatedCourse
+        );
+        console.log("Course updated successfully:", response.data);
+        // Reset the updated course state
+        const updatedCoursesCopy = { ...updatedCourses };
+        delete updatedCoursesCopy[courseId];
+        setUpdatedCourses(updatedCoursesCopy);
+      } catch (error) {
+        console.error("Error updating course:", error);
+      }
+    }
+  };
+
   return (
     <div className="container-dash">
       <Sidebar />
@@ -47,11 +106,166 @@ export default function CourseEdit() {
           ) : (
             <>
               <h2>{user.firstName + " " + user.lastName} courses</h2>
-              <ul className="flex">
+              <ul className={`flex ${expandedCourse ? 'flex-col' : 'flex-row'}`}>
                 {courses.map((course) => (
-                  <li key={course._id} className="flex-1 shadow-2xl bg-white rounded-2 m-2 w-16/32">
-                    <h3 className="lg:text-2xl w-full">{course.name}</h3>
-                    <p>{course.description}</p>
+                  <li
+                    key={course._id}
+                    className={`flex-1 shadow-2xl bg-white rounded-2 m-2 ${
+                      expandedCourse !== course._id ? "w-16/32" : "w-full"
+                    }`}
+                  >
+                    <input
+                      type="text"
+                      name="title"
+                      value={updatedCourses[course._id] ? updatedCourses[course._id].name : course.name}
+                      onChange={(e) => {
+                        const updatedCourseCopy = { ...updatedCourses[course._id] };
+                        updatedCourseCopy.name = e.target.value;
+                        setUpdatedCourses({ ...updatedCourses, [course._id]: updatedCourseCopy });
+                      }}
+                      className="lg:text-2xl w-full border-b-2 border-blue-500 focus:outline-none"
+                    />
+                    <textarea
+                      name="description"
+                      value={
+                        updatedCourses[course._id]
+                          ? updatedCourses[course._id].description
+                          : course.description
+                      }
+                      onChange={(e) => {
+                        const updatedCourseCopy = { ...updatedCourses[course._id] };
+                        updatedCourseCopy.description = e.target.value;
+                        setUpdatedCourses({ ...updatedCourses, [course._id]: updatedCourseCopy });
+                      }}
+                      className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                    ></textarea>
+                    {expandedCourse === course._id ? (
+                      <div>
+                        <h4 className="text-xl font-semibold mb-2">
+                          Course Content
+                        </h4>
+                        {course.content.map((chapter, chapterIndex) => (
+                          <div key={chapterIndex} className="mb-4">
+                            <label className="block text-gray-700 font-bold mb-2">
+                              Chapter Title:
+                            </label>
+                            <input
+                              type="text"
+                              name="chapterTitle"
+                              value={
+                                updatedCourses[course._id]
+                                  ? updatedCourses[course._id].content[chapterIndex].title
+                                  : chapter.title
+                              }
+                              onChange={(e) => {
+                                const updatedCourseCopy = { ...updatedCourses[course._id] };
+                                if (!updatedCourseCopy.content) {
+                                  updatedCourseCopy.content = [...course.content];
+                                }
+                                if (!updatedCourseCopy.content[chapterIndex]) {
+                                  updatedCourseCopy.content[chapterIndex] = { ...course.content[chapterIndex] };
+                                }
+                                updatedCourseCopy.content[chapterIndex].title = e.target.value;
+                                setUpdatedCourses({ ...updatedCourses, [course._id]: updatedCourseCopy });
+                              }}
+                              className="text-lg font-semibold w-full border-b-2 border-blue-500 focus:outline-none"
+                            />
+                            {chapter.lessons.map((lesson, lessonIndex) => (
+                              <div key={lessonIndex} className="mb-2">
+                                <label className="block text-gray-700 font-bold mb-2">
+                                  Subtitle:
+                                </label>
+                                <input
+                                  type="text"
+                                  name="subtitle"
+                                  value={
+                                    updatedCourses[course._id]
+                                      ? updatedCourses[course._id].content[chapterIndex].lessons[lessonIndex].subtitle
+                                      : lesson.subtitle
+                                  }
+                                  onChange={(e) => {
+                                    const updatedCourseCopy = { ...updatedCourses[course._id] };
+                                    if (!updatedCourseCopy.content) {
+                                      updatedCourseCopy.content = [...course.content];
+                                    }
+                                    if (!updatedCourseCopy.content[chapterIndex]) {
+                                      updatedCourseCopy.content[chapterIndex] = { ...course.content[chapterIndex] };
+                                    }
+                                    if (!updatedCourseCopy.content[chapterIndex].lessons[lessonIndex]) {
+                                      updatedCourseCopy.content[chapterIndex].lessons[lessonIndex] = { ...lesson };
+                                    }
+                                    updatedCourseCopy.content[chapterIndex].lessons[lessonIndex].subtitle =
+                                      e.target.value;
+                                    setUpdatedCourses({ ...updatedCourses, [course._id]: updatedCourseCopy });
+                                  }}
+                                  className="w-6/12 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                                />
+                                <label className="block text-gray-700 font-bold mb-2">
+                                  Lesson Content:
+                                </label>
+                                <textarea
+                                  name="content"
+                                  value={
+                                    updatedCourses[course._id]
+                                      ? updatedCourses[course._id].content[chapterIndex].lessons[lessonIndex].content
+                                      : lesson.content
+                                  }
+                                  onChange={(e) => {
+                                    const updatedCourseCopy = { ...updatedCourses[course._id] };
+                                    if (!updatedCourseCopy.content) {
+                                      updatedCourseCopy.content = [...course.content];
+                                    }
+                                    if (!updatedCourseCopy.content[chapterIndex]) {
+                                      updatedCourseCopy.content[chapterIndex] = { ...course.content[chapterIndex] };
+                                    }
+                                    if (!updatedCourseCopy.content[chapterIndex].lessons[lessonIndex]) {
+                                      updatedCourseCopy.content[chapterIndex].lessons[lessonIndex] = { ...lesson };
+                                    }
+                                    updatedCourseCopy.content[chapterIndex].lessons[lessonIndex].content =
+                                      e.target.value;
+                                    setUpdatedCourses({ ...updatedCourses, [course._id]: updatedCourseCopy });
+                                  }}
+                                  className="w-6/12 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                                ></textarea>
+                                <label className="block text-gray-700 font-bold mb-2">
+                                  Lesson Image:
+                                </label>
+                                <input
+                                  type="file"
+                                  accept="image/*"
+                                  onChange={(e) =>
+                                    handleChangeImage(
+                                      e,
+                                      course._id,
+                                      chapterIndex,
+                                      lessonIndex
+                                    )
+                                  }
+                                  className="w-6/12 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                                />
+                                {lesson.image && (
+                                  <img
+                                    src={`/courseimages/${lesson.image}`}
+                                    alt={lesson.subtitle}
+                                    className="mt-2 max-w-xs"
+                                  />
+                                )}
+                              </div>
+                            ))}
+                          </div>
+                        ))}
+                      </div>
+                    ) : (
+                      <>
+                        <button onClick={() => toggleCourseContent(course)}>
+                          Show Course Content
+                        </button>
+                        <button onClick={handleCloseCourseContent}>Cancel</button>
+                      </>
+                    )}
+                    <button onClick={() => handleUpdateCourse(course._id)}>
+                      Update Course
+                    </button>
                   </li>
                 ))}
               </ul>
@@ -60,9 +274,5 @@ export default function CourseEdit() {
         </div>
       </div>
     </div>
-
-
-
-
   );
 }

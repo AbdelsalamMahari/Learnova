46a4406b79728da1badb76e6ba0d000aa0152dea
Abdelsalam Mahari <abdelsalam.mahari@gmail.com>
2023-09-27 00:29:42 +0300

    update

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -26,7 +26,7 @@ export default function Home() {
     Axios.get("http://localhost:5000/courses")
       .then((response) => {
         // Get the first 3 courses
-        const first3Courses = response.data.slice(0, 3);
+        const first3Courses = response.data;
         setCourses(first3Courses);
       })
       .catch((error) => {
